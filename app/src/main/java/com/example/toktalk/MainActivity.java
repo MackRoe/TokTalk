65cd80346c16cbdf62d39d95839eae8b79872d97
MackRoe <elaine.music@students.makeschool.com>
2021-03-16 18:55:04 -0400

    modify MainActivity.java to handle messages to database

--- app/src/main/java/com/example/toktalk/MainActivity.java
+++ app/src/main/java/com/example/toktalk/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.toktalk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editMessage;
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editMessage = (EditText) findViewById(R.id.editMessageE);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Messages");
+    }
+
+    public void sendButtonClicked(View view ){
+        final String messageValue = editMessage.getText().toString().trim();
+        if (!TextUtils.isEmpty(messageValue)){
+            final DatabaseReference newPost = mDatabase.push();
+            newPost.child("content").setValue(messageValue);
+
+        }
     }
 }
